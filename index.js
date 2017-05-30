23b08e76f314f5b09abc03ca182396bf1cd897f1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-05-30 11:56:56 -0400

    ip6

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const server = http.createServer((req, res) => {
   res.writeHead(200)
   res.end()
 })
-const ip = '0.0.0.0'
+const ip = '::' // '0.0.0.0'
 const port = 6500
 server.listen(port, ip, () => {
   console.log('listening at %s:%d', ip, port)
+  console.log(server.address())
 })
